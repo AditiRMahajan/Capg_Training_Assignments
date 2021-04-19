c232889b5c5e9e1660fd081c2b2c8aee0347581d
AditiRMahajan <49728639+AditiRMahajan@users.noreply.github.com>
2021-04-19 21:06:12 +0530

    Update SecurityjpaApplication.java

--- Spring_Security/securityjpa/src/main/java/com/springsecurityjpa/securityjpa/SecurityjpaApplication.java
+++ Spring_Security/securityjpa/src/main/java/com/springsecurityjpa/securityjpa/SecurityjpaApplication.java
@@ -2,8 +2,11 @@ package com.springsecurityjpa.securityjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.securityExample.jpa.repository.UserRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SecurityjpaApplication {
 
 	public static void main(String[] args) {
